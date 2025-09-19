d2e2a74bce511ea6daa4bc92d235e107c7e45501
leehzitha <leticia.burlinski.com>
2025-09-19 20:42:43 -0300

    Botao aleatorio

--- Prática JS/cores.js
+++ Prática JS/cores.js
@@ -9,6 +9,15 @@ function changeBackgroundColor(color){
 // const whiteButton = document.getElementById("white");
 
 const colorButtons = document.getElementsByClassName("color-button");
+const randomButton = document.getElementById("random-button");
+
+randomButton.addEventListener("click", () => {
+    const r = Math.floor((Math.random() * 256));
+    const g = Math.floor((Math.random() * 256));
+    const b = Math.floor((Math.random() * 256));
+
+    changeBackgroundColor(`rgb(${r}, ${g}, ${b})`);
+});
 
 for (let i = 0; i < colorButtons.length; i++){
     colorButtons[i].addEventListener("click", (ev) => {changeBackgroundColor(ev.target.dataset.color)});
